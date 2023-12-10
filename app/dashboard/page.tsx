bcbce54561844c022a6232afa742a1642a272767
inoy <7m7110123@gmail.com>
2023-12-10 13:47:47 +0900

    /dashboardへCardコンポーネントで以下を表示 - Collectedの総額: us$ - Pending: us$ - Total Invoices: count - Total Customers: count

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,20 @@
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
+import {
+  fetchCardData,
+  fetchLatestInvoices,
+  fetchRevenue,
+} from '@/app/lib/data';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Card } from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
 
@@ -12,7 +23,20 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div></div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="collected" />
+        <Card
+          title="Total Invoices"
+          value={numberOfInvoices}
+          type="collected"
+        />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
