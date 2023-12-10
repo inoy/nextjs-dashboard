5e9dfa3e48d1d9e0b579524cd17b1752ae89e76b
inoy <7m7110123@gmail.com>
2023-12-10 13:21:39 +0900

    /dashboardへrevenueのチャート表示を追加

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div></div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
